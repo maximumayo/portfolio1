ec4e05bb5ce99a35d7cd7f8f3007aa68b56ae860
maximumayo <maxgmayo@gmail.com>
2016-04-07 21:02:10 -0700

    back to require_once

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require('email_config.php');
+require_once('email_config.php');
 require('PHPMailerAutoload.php');
 $mail = new PHPMailer();
 $mail->SMTPDebug = 3;                                   // Enable verbose debug output
